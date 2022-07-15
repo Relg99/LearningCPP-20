4f0cd9db80d3a4a3d1729392627a6237a8a1de65
Relg99 <relg1999@gmail.com>
2022-07-15 14:39:28 -0500

    End of excercise, Postfix and Prefix

--- main.cpp
+++ main.cpp
@@ -1,11 +1,36 @@
 /*
  * Coded by Relg99.
- * Date 28/06/22
+ * Date 15/07/22
+ * Compiled using Gcc v10.
  */
 
 #include <iostream>
 
 int main() {
-    auto result = (10 <=> 20) > 0;
-    std::cout << result << std::endl;
+    int value = {5};
+
+    // Here we can see the functionality of using Postfix.
+
+    std::cout << "The value is incrementing " << value++ << std::endl; // 5
+    std::cout << "The value " << value << std::endl; // 6
+
+    value = 5;
+
+    std::cout << "The value is decreasing " << value-- << std::endl; // 5
+    std::cout << "The value " << value << std::endl; // 4
+
+
+    // Here we can see the functionality of uisng Prefix.
+
+    value = 5;
+
+    std::cout << "The value already incremented " << ++value << std::endl; // 6
+    std::cout << "The same value " << value << std::endl;
+
+    value = 5;
+
+    std::cout << "The value already decreased " << --value << std::endl;
+    std::cout << "The same value " << value << std::endl;
+
+    return 0;   
 }
